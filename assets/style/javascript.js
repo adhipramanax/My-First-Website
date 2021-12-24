0a201c3a1a9c638b5ff09c8826842e8ae4638785
PANJI ADHIPRAMANA <57203669+adhipramanax@users.noreply.github.com>
2021-12-25 00:12:29 +0700

    Add files via upload

---
+++ assets/style/javascript.js
@@ -0,0 +1,8 @@
+//javascript
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
+alert("SELAMAT DATANG");
+var firstName = "panji";
+console.log(firstName);
+ 
+firstName = "adhipramana";
+console.log(firstName);
